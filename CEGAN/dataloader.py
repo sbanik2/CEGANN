e9a3fa2c94b00b43e53965834b63fb5722721b4c
sbanik2 <sbanik2@uic.edu>
2022-07-21 22:15:00 -0700

    Modifications

--- CEGAN/dataloader.py
+++ CEGAN/dataloader.py
@@ -1,22 +1,21 @@
-import torch
+from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
-from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
+# -------------Dataloader similar to CGCNN code---------------------
 
 
-#-------------Dataloader from CGCNN code---------------------
-
-
-def get_train_val_test_loader(dataset, 
-                              collate_fn=default_collate,
-                              batch_size=64,
-                              train_ratio=None,
-                              val_ratio=0.1, 
-                              test_ratio=0.1, 
-                              num_workers=1,
-                              pin_memory=False, 
-                              **kwargs):
+def get_train_val_test_loader(
+    dataset,
+    collate_fn=default_collate,
+    batch_size=64,
+    train_ratio=None,
+    val_ratio=0.1,
+    test_ratio=0.1,
+    num_workers=1,
+    pin_memory=False,
+    **kwargs,
+):
     """
     Utility function for dividing a dataset to train, val, test datasets.
 
@@ -48,18 +47,19 @@ def get_train_val_test_loader(dataset,
         return_test=True.
     """
     total_size = dataset.size
-    
-    
+
     if kwargs['train_size'] is None:
         if train_ratio is None:
             assert val_ratio + test_ratio < 1
             train_ratio = 1 - val_ratio - test_ratio
-            print(f'[Warning] train_ratio is None, using 1 - val_ratio - '
-                  f'test_ratio = {train_ratio} as training data.')
+            print(
+                f'[Warning] train_ratio is None, using 1 - val_ratio - '
+                f'test_ratio = {train_ratio} as training data.'
+            )
         else:
             print(train_ratio + val_ratio + test_ratio)
             assert train_ratio + val_ratio + test_ratio <= 1
-            
+
     indices = list(range(total_size))
     if kwargs['train_size'] is not None:
         train_size = kwargs['train_size']
@@ -73,35 +73,45 @@ def get_train_val_test_loader(dataset,
         valid_size = kwargs['val_size']
     else:
         valid_size = int(val_ratio * total_size)
-        
-        
+
     train_sampler = SubsetRandomSampler(indices[:train_size])
-    
-    
-    if test_size==0:
+
+    if test_size == 0:
         val_sampler = SubsetRandomSampler(indices[-valid_size:])
-        
+
     else:
         val_sampler = SubsetRandomSampler(
-        indices[-(valid_size + test_size):-test_size])
+            indices[-(valid_size + test_size) : -test_size]
+        )
         test_sampler = SubsetRandomSampler(indices[-test_size:])
-        
-    
-    train_loader = DataLoader(dataset, batch_size=batch_size,
-                              sampler=train_sampler,
-                              num_workers=num_workers,
-                              collate_fn=collate_fn, pin_memory=pin_memory)
-    val_loader = DataLoader(dataset, batch_size=batch_size,
-                            sampler=val_sampler,
-                            num_workers=num_workers,
-                            collate_fn=collate_fn, pin_memory=pin_memory)
-    
-    if test_size==0:
+
+    train_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=train_sampler,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+    )
+    val_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        sampler=val_sampler,
+        num_workers=num_workers,
+        collate_fn=collate_fn,
+        pin_memory=pin_memory,
+    )
+
+    if test_size == 0:
         test_loader = []
     else:
-        test_loader = DataLoader(dataset, batch_size=batch_size,
-                                     sampler=test_sampler,
-                                     num_workers=num_workers,
-                                     collate_fn=collate_fn, pin_memory=pin_memory)
+        test_loader = DataLoader(
+            dataset,
+            batch_size=batch_size,
+            sampler=test_sampler,
+            num_workers=num_workers,
+            collate_fn=collate_fn,
+            pin_memory=pin_memory,
+        )
 
     return train_loader, val_loader, test_loader
--- CEGAN/settings.py
+++ CEGAN/settings.py
@@ -1,31 +1,25 @@
-import os
-from pydantic.typing import Literal
 from typing import Optional
+
 from pydantic import BaseModel
-
-
+from pydantic.typing import Literal
 
 
 class Settings(BaseModel):
-    
-    #----------------------------------------------------
-    search_type: Literal["local","global"] = "local"
-    
-    
+
+    # ----------------------------------------------------
+    search_type: Literal["local", "global"] = "local"
+
     POOL = {"local": False, "global": True}
 
+    # ---------------------Graph creation--------------------
 
-    #---------------------Graph creation--------------------
-    
-    
     neighbors: int = 12
     rcut: float = 3.0
     search_delta: float = 1.0
     n_classification: int = 2
-    
-    #--------------dataloader parameter--------------------
-    
-    
+
+    # --------------dataloader parameter--------------------
+
     train_size: Optional[int] = None
     test_size: Optional[int] = None
     val_size: Optional[int] = None
@@ -36,49 +30,39 @@ class Settings(BaseModel):
     num_workers: int = 1
     pin_memory: bool = False
     batch_size: int = 64
-    
-    
-    #----------------- model training parameters-----------------
-    
-    
+
+    # ----------------- model training parameters-----------------
+
     bond_fea_len: int = 80
     angle_fea_len: int = 80
     n_conv_edge: int = 3
-   
-    
-    gbf_bond: dict = {'dmin':0, 'dmax':8, 'steps':bond_fea_len}
-    gbf_angle: dict = {'dmin':-1, 'dmax':1, 'steps': angle_fea_len}
-    
-    
-    h_fea_edge: int = 128                    # hidden feature len
-    h_fea_angle: int = 128 
-                                             # Number of hidden layer 
-        
-        
+
+    gbf_bond: dict = {'dmin': 0, 'dmax': 8, 'steps': bond_fea_len}
+    gbf_angle: dict = {'dmin': -1, 'dmax': 1, 'steps': angle_fea_len}
+
+    h_fea_edge: int = 128  # hidden feature len
+    h_fea_angle: int = 128
+    # Number of hidden layer
+
     @property
     def pooling(self):
         return self.POOL[self.search_type]
-                  
-    embedding : bool = False
-    checkpoint_every : int = 1
-    
-    
-    #----------------- model run parameters-----------------
-    
-    
+
+    embedding: bool = False
+    checkpoint_every: int = 1
+
+    # ----------------- model run parameters-----------------
+
     resume: bool = False
     epochs: int = 100
     optimizer: Literal["adam", "sgd"] = "adam"
     weight_decay: float = 0
     momentum: float = 0.9
     learning_rate: float = 1e-2
-    
+
     scheduler: bool = True
     gamma: float = 0.1
     step_size: int = 30
-    
 
     write_checkpoint: bool = True
     progress: bool = True
-
-    
